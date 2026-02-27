aae7a9a20a051bb35529254b01f913aead0b9d4f
Samy <99757181+samy-bennabi@users.noreply.github.com>
2026-02-26 21:18:02 -0500

    Added powershell autoclick script. what better place to keep it other than here. should make a bash one as well.

---
+++ moda_clika.ps1
@@ -0,0 +1,8 @@
+﻿Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32 -Namespace W;
+$imma_click_this_many_times = 20000
+$imma_wait_this_much_between_clicks = 5 #dis very short time less dan second.
+Start-Sleep -Seconds 1
+for($i=0; $i -le $imma_click_this_many_times; $i++){
+    [W.U32]::mouse_event(6,0,0,0,0);
+    Start-Sleep -Milliseconds $imma_wait_this_much_between_clicks
+}
