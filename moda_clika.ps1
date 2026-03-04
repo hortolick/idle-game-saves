6e4e52c213941d7456d016f15a28c406992190e5
horto <90649770+hortolick@users.noreply.github.com>
2026-03-04 18:05:50 -0500

    removed comment from scripp

--- moda_clika.ps1
+++ moda_clika.ps1
@@ -1,5 +1,5 @@
 ﻿Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern void mouse_event(int flags, int dx, int dy, int cButtons, int info);' -Name U32 -Namespace W;
-$imma_click_this_many_times = 30*200 #change first number and it's aproximate (very inaccurate) seconds
+$imma_click_this_many_times = 30*200
 $imma_wait_this_much_between_clicks = 5 #dis very short time less dan second.
 Start-Sleep -Seconds 1
 for($i=0; $i -le $imma_click_this_many_times; $i++){
